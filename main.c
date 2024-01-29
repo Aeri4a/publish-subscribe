da7868c6c3c7af5606b20085cb34ced33f7d5c68
Aeri4a <80831299+Aeri4a@users.noreply.github.com>
2024-01-29 19:34:01 +0100

    added tests

--- main.c
+++ main.c
@@ -71,52 +71,102 @@ void *remover(void *q) {
 }
 
 int main() {
-    pthread_t pub1, pub2, pub3;
-    pthread_t sub1, sub2, sub3, sub4, sub5, sub6;
+    // TEST SECTION
+    // PLEASE UNCOMMENT CHOOSEN TEST
+
+    // # CASE 1 --------------------------------------------------
+    // 1 publisher, 4 subscribers
+    // Starting with one subscriber
+    // After 5 seconds, one subscriber (number 1) will be unsubscribed
+    // After next 7 seconds, four subscribers will be added
+    // After next 8 seconds, there will be called remover
+    // Starting size 4, after next 10 sec increasing size to 6
+    // Destroying queue after 60 seconds
+    // UNCOMMENT >>
+    printf("STARTING TEST CASE 1\n\n");
+    pthread_t pub1;
     pthread_t rem1;
+    pthread_t sub1, sub2, sub3, sub4, sub5;
 
     TQueue *queue = malloc(sizeof(TQueue));
-    createQueueI(queue, 5);
+    createQueueI(queue, 4);
 
     pthread_create(&pub1, NULL, publisher, queue);
-    pthread_create(&pub2, NULL, publisher, queue);
-    pthread_create(&pub3, NULL, publisher, queue);
     pthread_create(&sub1, NULL, subscriber, queue);
+    
+    sleep(5);
+    unsubscribeI(queue, sub1);
+
+    sleep(7);
     pthread_create(&sub2, NULL, subscriber, queue);
     pthread_create(&sub3, NULL, subscriber, queue);
     pthread_create(&sub4, NULL, subscriber, queue);
     pthread_create(&sub5, NULL, subscriber, queue);
-    pthread_create(&sub6, NULL, subscriber, queue);
-    // pthread_create(&rem1, NULL, remover, queue);
-    
 
-    // # Testing unsubscribing
-    // sleep(2);
-    // unsubscribeI(queue, sub1);
-    // sleep(2);
-    // unsubscribeI(queue, sub2);
-    // sleep(2);
-    // unsubscribeI(queue, sub3);
+    sleep(8);
+    pthread_create(&rem1, NULL, remover, queue);
 
-    // sleep(5);
-    // pthread_create(&sub4, NULL, subscriber, queue);
-
-    // sleep(30);
-    // setSizeI(queue, 5);
-    
     sleep(10);
+    setSizeI(queue, 6);
+    
+    sleep(60);
     destroyQueueI(queue);
 
     pthread_join(pub1, NULL);
-    pthread_join(pub2, NULL);
-    pthread_join(pub3, NULL);
+    pthread_join(rem1, NULL);
     pthread_join(sub1, NULL);
     pthread_join(sub2, NULL);
     pthread_join(sub3, NULL);
     pthread_join(sub4, NULL);
     pthread_join(sub5, NULL);
-    pthread_join(sub6, NULL);
-    // pthread_join(rem1, NULL);
+    // UNCOMMENT >>
+    // # -------------------------------------------------------------
+
+
+
+    // # Case 2 --------------------------------------------------
+    // 3 publishers, 6 subscribers
+    // After 3 seconds, one subscriber (number 1) will be unsubscribed
+    // Starting size 10, after next 5 sec reducing to size 5
+    // Destroying queue after next 60 seconds
+    // UNCOMMENT >>
+        // printf("STARTING TEST CASE 2\n\n");
+        // pthread_t pub1, pub2, pub3;
+        // pthread_t sub1, sub2, sub3, sub4, sub5, sub6;
+
+        // TQueue *queue = malloc(sizeof(TQueue));
+        // createQueueI(queue, 10);
+
+        // pthread_create(&pub1, NULL, publisher, queue);
+        // pthread_create(&pub2, NULL, publisher, queue);
+        // pthread_create(&pub3, NULL, publisher, queue);
+        // pthread_create(&sub1, NULL, subscriber, queue);
+        // pthread_create(&sub2, NULL, subscriber, queue);
+        // pthread_create(&sub3, NULL, subscriber, queue);
+        // pthread_create(&sub4, NULL, subscriber, queue);
+        // pthread_create(&sub5, NULL, subscriber, queue);
+        // pthread_create(&sub6, NULL, subscriber, queue);
+        
+        // sleep(3);
+        // unsubscribeI(queue, sub1);
+
+        // sleep(5);
+        // setSizeI(queue, 5);
+        
+        // sleep(60);
+        // destroyQueueI(queue);
+
+        // pthread_join(pub1, NULL);
+        // pthread_join(pub2, NULL);
+        // pthread_join(pub3, NULL);
+        // pthread_join(sub1, NULL);
+        // pthread_join(sub2, NULL);
+        // pthread_join(sub3, NULL);
+        // pthread_join(sub4, NULL);
+        // pthread_join(sub5, NULL);
+        // pthread_join(sub6, NULL);
+    // UNCOMMENT >>
+    // # -------------------------------------------------------------
 
     return 0;
 }
